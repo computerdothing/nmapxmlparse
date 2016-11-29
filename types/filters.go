da8f496050a291f9c0d12d8770eb4628c1b3bfa6
Andrew Carlson-Lynch <acarlsonlynch@liquidweb.com>
2016-11-29 18:01:57 -0500

    * Little more

--- types/filters.go
+++ types/filters.go
@@ -1,22 +1,26 @@
 package types
 
-type HostFilter struct {}
+type HostFilter interface {
+	Pass(*Host) bool
+}
 
 type HostFilterChain []HostFilter
 
 func (nmr NmapRun) Filter(fc HostFilterChain) NmapRun {
-  var rv NmapRun
+	var rv NmapRun
 
-  // copy meta stuff over since we're just filtering hosts
-  rv.Scanner = nmr.Scanner
-  rv.Args = nmr.Args
-  rv.Start = nmr.Start
-  rv.StartStr = nmr.StartStr
-  rv.Version = nmr.Version
-  rv.XmlOutputVersion = nmr.XmlOutputVersion
-  rv.ScanInfo = nmr.ScanInfo
-  rv.Verbose = nmr.Verbose
-  rv.Debugging = nmr.Debugging
+	// copy meta stuff over to rv since we're just filtering hosts
+	rv.Scanner = nmr.Scanner
+	rv.Args = nmr.Args
+	rv.Start = nmr.Start
+	rv.StartStr = nmr.StartStr
+	rv.Version = nmr.Version
+	rv.XmlOutputVersion = nmr.XmlOutputVersion
+	rv.ScanInfo = nmr.ScanInfo
+	rv.Verbose = nmr.Verbose
+	rv.Debugging = nmr.Debugging
 
+	for _, f := range fc {
 
+	}
 }
