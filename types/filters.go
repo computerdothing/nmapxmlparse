0a10392a35a8833aea0805e4204704bf6e41f638
Andrew Carlson-Lynch <acarlsonlynch@liquidweb.com>
2016-11-30 19:22:19 -0500

    * Compiles. Have to test next.

--- types/filters.go
+++ types/filters.go
@@ -1,26 +1,70 @@
 package types
 
+import (
+	"nmapxmlparse/util"
+)
+
 type HostFilter interface {
 	Pass(*Host) bool
 }
 
-type HostFilterChain []HostFilter
+func (nmr NmapRun) Filter(filters ...HostFilter) error {
+	var finalForm []Host
 
-func (nmr NmapRun) Filter(fc HostFilterChain) NmapRun {
-	var rv NmapRun
-
-	// copy meta stuff over to rv since we're just filtering hosts
-	rv.Scanner = nmr.Scanner
-	rv.Args = nmr.Args
-	rv.Start = nmr.Start
-	rv.StartStr = nmr.StartStr
-	rv.Version = nmr.Version
-	rv.XmlOutputVersion = nmr.XmlOutputVersion
-	rv.ScanInfo = nmr.ScanInfo
-	rv.Verbose = nmr.Verbose
-	rv.Debugging = nmr.Debugging
-
-	for _, f := range fc {
-
+	for _, h := range nmr.Hosts {
+		good := true
+		for _, f := range filters {
+			if !f.Pass(&h) {
+				good = false
+				break
+			}
+		}
+		if good {
+			finalForm = append(finalForm, h)
+		}
 	}
+
+	nmr.Hosts = finalForm
+
+	return nil
 }
+
+//// ANY OPEN PORT ////
+type anyOpenPort struct{}
+
+func (hop anyOpenPort) Pass(h *Host) bool {
+	for _, p := range h.Ports {
+		if p.State.State == "open" {
+			return true
+		}
+	}
+	return false
+}
+
+func AnyOpenPort() HostFilter {
+	return &anyOpenPort{}
+}
+
+///////////////////////
+
+//// SELECT OPEN PORTS ////
+type selectOpenPorts struct {
+	portIds []int
+}
+
+func (sop selectOpenPorts) Pass(h *Host) bool {
+	for _, p := range h.Ports {
+		if util.ContainsInt(sop.portIds, p.Portid) {
+			if p.State.State == "open" {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func SelectOpenPorts(ports ...int) HostFilter {
+	return &selectOpenPorts{ports}
+}
+
+///////////////////////////
